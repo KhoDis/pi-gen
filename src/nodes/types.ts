44044c129d915190875145e7e9670a0c5988cb10
KhoDis <khodzhayarov.a@gmail.com>
2025-05-19 17:39:08 +0300

    build: add style packages

--- src/nodes/types.ts
+++ src/nodes/types.ts
@@ -1,4 +1,34 @@
-import type { Node, BuiltInNode } from "@xyflow/react";
+import { CircleNodeData } from "@/nodes/CircleNode.tsx";
+import { BuiltInNode, type Node } from "@xyflow/react";
+
+export type ConnectionType = "layer" | "color" | "number" | "option";
+
+export type OutputNodeParams = {
+  width: number;
+  height: number;
+};
+
+export type Connection = {
+  id: string;
+  source: string;
+  sourceHandle: string;
+  target: string;
+  targetHandle: string;
+};
+
+export type CanvasNodeData = {
+  width: number;
+  height: number;
+  // Optional initial draw function
+  scale: number;
+  onDraw?: (ctx: CanvasRenderingContext2D) => void;
+};
 
 export type PositionLoggerNode = Node<{ label: string }, "position-logger">;
-export type AppNode = BuiltInNode | PositionLoggerNode;
+export type CanvasNode = Node<CanvasNodeData, "canvas">;
+export type CircleNode = Node<CircleNodeData, "circle">;
+export type AppNode =
+  | BuiltInNode
+  | PositionLoggerNode
+  | CanvasNode
+  | CircleNode;
